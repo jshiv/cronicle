039540c5fffb6b148a704aa14011d1e18537b762
Jason Shiverick <jshiv00@gmail.com>
2019-08-18 14:38:49 -0700

    Added simple cron scheduler

---
+++ pkg/cron/main.go
@@ -0,0 +1,18 @@
+package cron
+
+import (
+	"runtime"
+	log "github.com/sirupsen/logrus"
+	"gopkg.in/robfig/cron.v2"
+	//https://github.com/distribworks/dkron
+)
+
+//RunSchedule starts cron
+func RunSchedule() {
+	log.WithFields(log.Fields{"cronicle": "start"}).Info("Starting Scheduler...")
+
+	c := cron.New()
+	c.AddFunc("@every 10s", func() { log.WithFields(log.Fields{"cronicle": "core"}).Info("Running...") })
+	c.Start() // Stop the scheduler (does not stop any jobs already running).
+	runtime.Goexit()
+}
