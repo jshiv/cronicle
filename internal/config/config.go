164a8ee193cf31eb9b178494869d8a492e551a7a
Jason Shiverick <jason.shiverick@udemy.com>
2019-09-10 10:36:41 -0700

    Added parse_test.go

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,29 +1,17 @@
-
 package config
 
-
-// Config is the configuration structure for the license checker.
+// Config is the configuration structure for the cronicle checker.
 // https://raw.githubusercontent.com/mitchellh/golicense/master/config/config.go
-
 type Config struct {
-	// Allow and Deny are the list of licenses that are allowed or disallowed,
-	// respectively. The string value here can be either the license name
-	// (case insensitive) or the SPDX ID (case insensitive).
-	//
-	// If a license is found that isn't in either list, then a warning is
-	// emitted. If a license is in both deny and allow, then deny takes
-	// priority.
-	Allow []string `hcl:"allow,optional"`
-	Deny  []string `hcl:"deny,optional"`
+	Version  string `hcl:"version,optional"`
+	Schedule Schedule
+}
 
-	// Override is a map that explicitly sets the license for the given
-	// import path. The key is an import path (exact) and the value is
-	// the name or SPDX ID of the license. Regardless, the value will
-	// be set as both the name and SPDX ID, so SPDX IDs are recommended.
-	Override map[string]string `hcl:"override,optional"`
-
-	// Translate is a map that translates one import source into another.
-	// For example, "gopkg.in/(.*)" => "github.com/\1" would translate
-	// gopkg into github (incorrectly, but the example would work).
-	Translate map[string]string `hcl:"translate,optional"`
+// Schedule is the configuration structure that defines a single cron job.
+type Schedule struct {
+	// Cron is the schedule interval. The field accepts standard cron
+	// and other configurations listed here https://godoc.org/gopkg.in/robfig/cron.v2
+	// i.e. ["@hourly", "@every 1h30m", "0 30 * * * *", "TZ=Asia/Tokyo 30 04 * * * *"]
+	Cron    string `hcl:"cron,optional"`
+	Command string `hcl:"command,optional"`
 }
---
+++ internal/config/parse_test.go
@@ -0,0 +1,12 @@
+package config_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	. "UNKNOWN_PACKAGE_PATH"
+)
+
+var _ = Describe("Parse", func() {
+
+})
