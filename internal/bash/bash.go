d11e5799315de77e6bad4580075bd8ff0518ca53
Jason Shiverick <jason.shiverick@udemy.com>
2019-09-11 13:54:19 -0700

    Updated cmd input from string to []string

--- internal/bash/bash.go
+++ internal/bash/bash.go
@@ -11,16 +11,16 @@ import (
 //BashRun pulls from examples at https://zaiste.net/executing_external_commands_in_go/
 // and //https://gist.github.com/mchirico/6045501
 
-type Data struct {
+type Result struct {
 	Command    []string
 	Stdout     string
 	Stderr     string
 	ExitStatus int
 }
 
-func Bash(command []string) Data {
-	var data Data
-	data.Command = command
+func Bash(command []string) Result {
+	var result Result
+	result.Command = command
 	var cmd *exec.Cmd
 	switch len(command) {
 	case 1:
@@ -40,11 +40,11 @@ func Bash(command []string) Data {
 
 	bb := bytes.NewBuffer([]byte{})
 	_, err = bb.ReadFrom(stdout)
-	data.Stdout = bb.String()
+	result.Stdout = bb.String()
 
 	be := bytes.NewBuffer([]byte{})
 	_, err = be.ReadFrom(stderr)
-	data.Stderr = be.String()
+	result.Stderr = be.String()
 
 	var waitStatus syscall.WaitStatus
 	if err := cmd.Wait(); err != nil {
@@ -53,18 +53,18 @@ func Bash(command []string) Data {
 		}
 		if exitError, ok := err.(*exec.ExitError); ok {
 			waitStatus = exitError.Sys().(syscall.WaitStatus)
-			data.ExitStatus = waitStatus.ExitStatus()
+			result.ExitStatus = waitStatus.ExitStatus()
 		}
 	} else {
 		// Success
 		waitStatus = cmd.ProcessState.Sys().(syscall.WaitStatus)
-		data.ExitStatus = waitStatus.ExitStatus()
+		result.ExitStatus = waitStatus.ExitStatus()
 	}
-	return data
+	return result
 }
 
-func LogStdout(data Data) {
+func LogStdout(result Result) {
 	log.WithFields(log.Fields{
-		"bash": data.Command,
-	}).Info(data.Stdout)
+		"bash": result.Command,
+	}).Info(result.Stdout)
 }
