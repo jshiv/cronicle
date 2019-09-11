a13e0e3597f5a9a6b9b05e2c9b47dcedb1ba9ff1
pauljcho <paul.cho@udemy.com>
2019-09-11 12:11:18 -0700

    first pass at config schema and first test

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/jshiv/cronicle
 go 1.13
 
 require (
-	github.com/hashicorp/hcl2 v0.0.0-20190821123243-0c888d1241f6
+	github.com/hashicorp/hcl2 v0.0.0-20190909202536-66c59f909e25
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/onsi/ginkgo v1.10.1
 	github.com/onsi/gomega v1.4.3
--- internal/config/config.go
+++ internal/config/config.go
@@ -4,14 +4,36 @@ package config
 // https://raw.githubusercontent.com/mitchellh/golicense/master/config/config.go
 type Config struct {
 	Version  string `hcl:"version,optional"`
-	Schedule Schedule
+	Git string `hcl:"git"`
+	Schedules []Schedule `hcl:"schedule,optional"`
 }
 
-// Schedule is the configuration structure that defines a single cron job.
+// Schedule is the configuration structure that defines a cron job consisting of tasks.
 type Schedule struct {
 	// Cron is the schedule interval. The field accepts standard cron
 	// and other configurations listed here https://godoc.org/gopkg.in/robfig/cron.v2
 	// i.e. ["@hourly", "@every 1h30m", "0 30 * * * *", "TZ=Asia/Tokyo 30 04 * * * *"]
+	Name string `hcl:"name,label"`
 	Cron    string `hcl:"cron,optional"`
-	Command string `hcl:"command,optional"`
+	StartDate string `hcl:"start_date,optional"`
+	EndDate string `hcl:"end_date,optional"`
+	// Owner Owner `hcl:"owner"`
+	Tasks []Task `hcl:"task,block"`
+}
+
+// Task is the configuration structure that defines a task (i.e., a command)
+type Task struct {
+	Name string `hcl:"name,label"`
+	Command []string `hcl:"command,optional"`
+	Depends []string `hcl:"depends,optional"`
+	Owner *Owner `hcl:"owner,block"`
+	Repo string `hcl:"repo,optional"`
+	Branch string `hcl:"branch,optional"`
+	Commit string `hcl:"commit,optional"`
+}
+
+// Owner is the configuration structure that defines an owner of a schedule or task
+type Owner struct {
+	Name string `hcl:"name"`
+	Email string `hcl:"email,optional"`
 }
