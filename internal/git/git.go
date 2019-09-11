b3602d6ded64d36bdd92fed8c529222878e632eb
Jessica Stewart <jessica.stewart@udemy.com>
2019-09-11 15:11:59 -0700

    Adds init

--- internal/git/git.go
+++ internal/git/git.go
@@ -1,4 +1,4 @@
-package main
+package git
 
 import (
 	"fmt"
@@ -9,24 +9,6 @@ import (
 	"gopkg.in/src-d/go-git.v4"
 	"gopkg.in/src-d/go-git.v4/plumbing/object"
 )
-
-// Init returns a new repository using the .git folder, if the fixture
-// is tagged as worktree the filesystem from fixture is used, otherwise a new
-// memfs filesystem is used as worktree.
-func Init(path string) *git.Repository {
-
-	repo, err := git.PlainOpen(path) //
-	if err != nil {
-		//this feels wrong :-(
-		repo, initErr := git.PlainInit(path, false)
-		if initErr != nil {
-			panic(initErr)
-		}
-		return repo
-	}
-	return repo
-
-}
 
 // CheckIfError should be used to naively panics if an error is not nil.
 func CheckIfError(err error) {
@@ -41,6 +23,12 @@ func CheckIfError(err error) {
 // Info should be used to describe the example commands that are about to run.
 func Info(format string, args ...interface{}) {
 	fmt.Printf("\x1b[34;1m%s\x1b[0m\n", fmt.Sprintf(format, args...))
+}
+
+func GitInit(path string) {
+	fs, err := git.PlainInit(path, true)
+	CheckIfError(err)
+	fmt.Println(fs)
 }
 
 func Clone(gitURL string, dir string) {
@@ -107,48 +95,26 @@ func GitLog(worktreeDir string) {
 
 	// ... just iterates over the commits, printing it
 	err = cIter.ForEach(func(c *object.Commit) error {
-		fmt.Println(c)  // commit as struct https://godoc.org/gopkg.in/src-d/go-git.v4/plumbing/object#Commit 
+		fmt.Println(c) // commit as struct https://godoc.org/gopkg.in/src-d/go-git.v4/plumbing/object#Commit
 		return nil
 	})
 	CheckIfError(err)
 }
 
-func main() {
-	// Clone("https://github.com/src-d/go-git.git", "/tmp/foo")
-	// Commit("/Users/jessicas/work/cronicle", "example go-git commit")
-	GitLog("/Users/jessicas/work/cronicle")
+func Push(worktreeDir string) {
+	// We instantiate a new repository targeting the given path (the .git folder)
+	r, err := git.PlainOpen(worktreeDir)
+	CheckIfError(err)
+
+	Info("git push")
+	// push using default options
+	err = r.Push(&git.PushOptions{}) // TODO: add auth
+	CheckIfError(err)
 }
 
-// // RunExample of how to:
-// // - Clone a repository into memory
-// // - Get the HEAD reference
-// // - Using the HEAD reference, obtain the commit this reference is pointing to
-// // - Using the commit, obtain its history and print it
-// func RunExample() {
-// 	// Clones the given repository, creating the remote, the local branches
-// 	// and fetching the objects, everything in memory:
-// 	Info("git clone https://github.com/src-d/go-siva")
-// 	r, err := git.Clone(memory.NewStorage(), nil, &git.CloneOptions{
-// 		URL: "https://github.com/src-d/go-siva",
-// 	})
-// 	CheckIfError(err)
-
-// 	// Gets the HEAD history from HEAD, just like this command:
-// 	Info("git log")
-
-// 	// ... retrieves the branch pointed by HEAD
-// 	ref, err := r.Head()
-// 	CheckIfError(err)
-
-// 	// ... retrieves the commit history
-// 	cIter, err := r.Log(&git.LogOptions{From: ref.Hash()})
-// 	CheckIfError(err)
-
-// 	// ... just iterates over the commits, printing it
-// 	err = cIter.ForEach(func(c *object.Commit) error {
-// 		fmt.Println(c)
-
-// 		return nil
-// 	})
-// 	CheckIfError(err)
-// }
+// Example usages:
+// Clone("https://github.com/src-d/go-git.git", "/tmp/foo")
+// Commit("/Users/jessicas/work/cronicle", "example go-git commit")
+// GitLog("/Users/jessicas/work/cronicle")
+// Push("/Users/jessicas/work/cronicle")
+// GitInit("/tmp/foo")
