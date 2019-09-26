26c26fb3a90fe901e6089edd11ebc2732f7a4026
Jason Shiverick <jshiv00@gmail.com>
2019-09-25 22:37:57 -0700

    Added init to config test suite

--- internal/config/config_suite_test.go
+++ internal/config/config_suite_test.go
@@ -1,13 +1,29 @@
 package config_test
 
 import (
+	"os"
 	"testing"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+
+	"path/filepath"
+
+	"github.com/jshiv/cronicle/internal/config"
 )
+
+var croniclePath string
 
 func TestConfig(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Config Suite")
 }
+
+var _ = BeforeSuite(func() {
+	croniclePath, _ := filepath.Abs("./testconfig")
+	config.Init(croniclePath)
+})
+
+var _ = AfterSuite(func() {
+	os.RemoveAll("./testconfig")
+})
--- internal/config/init_test.go
+++ internal/config/init_test.go
@@ -6,111 +6,101 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
-	"path/filepath"
-
 	"github.com/jshiv/cronicle/internal/config"
 )
 
 var _ = Describe("Init", func() {
-	// Describe("Get Repos from Config.Repos", func() {
-	// 	Context("config.GetRepos", func() {
-	// 		It("should be https://github.com/jshiv/cronicle-sample.git", func() {
-	// 			conf := config.Default()
-	// 			conf.Repos = []string{"https://github.com/jshiv/cronicle-sample.git"}
-	// 			repos := config.GetRepos(&conf)
-	// 			expected := map[string]bool{
-	// 				"https://github.com/jshiv/cronicle-sample.git": true,
-	// 			}
-	// 			Expect(repos).To(Equal(expected))
-	// 		})
-	// 	})
-	// })
-
-	// Describe("Get Repos from Config.Schedules[0].Repo", func() {
-	// 	Context("config.GetRepos", func() {
-	// 		It("should be https://github.com/jshiv/cronicle-sample.git", func() {
-	// 			conf := config.Default()
-	// 			conf.Schedules[0].Repo = "https://github.com/jshiv/cronicle-sample.git"
-	// 			repos := config.GetRepos(&conf)
-	// 			expected := map[string]bool{
-	// 				"https://github.com/jshiv/cronicle-sample.git": true,
-	// 			}
-	// 			Expect(repos).To(Equal(expected))
-	// 		})
-	// 	})
-	// })
-
-	// Describe("Get Repos from Config.Schedules[0].Tasks[0].Repo", func() {
-	// 	Context("config.GetRepos", func() {
-	// 		It("should be https://github.com/jshiv/cronicle-sample.git", func() {
-	// 			conf := config.Default()
-	// 			conf.Schedules[0].Tasks[0].Repo = "https://github.com/jshiv/cronicle-sample.git"
-	// 			repos := config.GetRepos(&conf)
-	// 			expected := map[string]bool{
-	// 				"https://github.com/jshiv/cronicle-sample.git": true,
-	// 			}
-	// 			Expect(repos).To(Equal(expected))
-	// 		})
-	// 	})
-	// })
-
-	// Describe("get the same repo from Config, Schedules and Tasks", func() {
-	// 	Context("config.GetRepos", func() {
-	// 		It("should be https://github.com/jshiv/cronicle-sample.git", func() {
-	// 			conf := config.Default()
-	// 			conf.Repos = []string{"https://github.com/jshiv/cronicle-sample.git"}
-	// 			conf.Schedules[0].Repo = "https://github.com/jshiv/cronicle-sample.git"
-	// 			conf.Schedules[0].Tasks[0].Repo = "https://github.com/jshiv/cronicle-sample.git"
-	// 			repos := config.GetRepos(&conf)
-	// 			expected := map[string]bool{
-	// 				"https://github.com/jshiv/cronicle-sample.git": true,
-	// 			}
-	// 			Expect(repos).To(Equal(expected))
-	// 		})
-	// 	})
-	// })
-
-	// Describe("get different repos from Config, Schedules and Tasks", func() {
-	// 	Context("config.GetRepos", func() {
-	// 		It("should be https://github.com/jshiv/cronicle-sample.git", func() {
-	// 			conf := config.Default()
-	// 			conf.Repos = []string{"https://github.com/jshiv/cronicle-sample.git"}
-	// 			conf.Schedules[0].Repo = "https://github.com/jshiv/cronicle-sample1.git"
-	// 			conf.Schedules[0].Tasks[0].Repo = "https://github.com/jshiv/cronicle-sample2.git"
-	// 			repos := config.GetRepos(&conf)
-	// 			expected := map[string]bool{
-	// 				"https://github.com/jshiv/cronicle-sample.git":  true,
-	// 				"https://github.com/jshiv/cronicle-sample1.git": true,
-	// 				"https://github.com/jshiv/cronicle-sample2.git": true,
-	// 			}
-	// 			Expect(repos).To(Equal(expected))
-	// 		})
-	// 	})
-	// })
-
 	Describe("Set Config given a basic config", func() {
 		Context("init.SetConfig", func() {
+			var conf config.Config
+			JustBeforeEach(func() {
+				conf = config.Default()
+			})
 			It("should be populated Path", func() {
-				var task config.Task
-				task.Name = "hello"
-				task.Command = []string{"/bin/echo", "Hello World"}
-
-				var schedule config.Schedule
-				schedule.Name = "example"
-				schedule.Cron = "@every 5s"
-				schedule.Tasks = []config.Task{task}
-
-				var conf config.Config
-				conf.Schedules = []config.Schedule{schedule}
-				path, _ := filepath.Abs("./testcron")
-				// Expect(path).To(Equal("1"))
-				err := config.SetConfig(&conf, path)
+				err := config.SetConfig(&conf, croniclePath)
 				if err != nil {
 					fmt.Println(err)
 				}
-				Expect(conf).To(Equal("1"))
-				Expect(conf.Schedules[0].Tasks[0].Path).To(Equal(path))
+				// Expect(conf).To(Equal("1"))
+				Expect(conf.Schedules[0].Tasks[0].Path).To(Equal(croniclePath))
 			})
 		})
 	})
+
+	Describe("Get Repos from Config.Repos", func() {
+		Context("config.GetRepos", func() {
+			It("should be https://github.com/jshiv/cronicle-sample.git", func() {
+				conf := config.Default()
+				conf.Repos = []string{"https://github.com/jshiv/cronicle-sample.git"}
+				repos := config.GetRepos(&conf)
+				expected := map[string]bool{
+					"https://github.com/jshiv/cronicle-sample.git": true,
+				}
+				Expect(repos).To(Equal(expected))
+			})
+		})
+	})
+
+	Describe("Get Repos from Config.Schedules[0].Repo", func() {
+		Context("config.GetRepos", func() {
+			It("should be https://github.com/jshiv/cronicle-sample.git", func() {
+				conf := config.Default()
+				conf.Schedules[0].Repo = "https://github.com/jshiv/cronicle-sample.git"
+				repos := config.GetRepos(&conf)
+				expected := map[string]bool{
+					"https://github.com/jshiv/cronicle-sample.git": true,
+				}
+				Expect(repos).To(Equal(expected))
+			})
+		})
+	})
+
+	Describe("Get Repos from Config.Schedules[0].Tasks[0].Repo", func() {
+		Context("config.GetRepos", func() {
+			It("should be https://github.com/jshiv/cronicle-sample.git", func() {
+				conf := config.Default()
+				conf.Schedules[0].Tasks[0].Repo = "https://github.com/jshiv/cronicle-sample.git"
+				repos := config.GetRepos(&conf)
+				expected := map[string]bool{
+					"https://github.com/jshiv/cronicle-sample.git": true,
+				}
+				Expect(repos).To(Equal(expected))
+			})
+		})
+	})
+
+	Describe("get the same repo from Config, Schedules and Tasks", func() {
+		Context("config.GetRepos", func() {
+			It("should be https://github.com/jshiv/cronicle-sample.git", func() {
+				conf := config.Default()
+				conf.Repos = []string{"https://github.com/jshiv/cronicle-sample.git"}
+				conf.Schedules[0].Repo = "https://github.com/jshiv/cronicle-sample.git"
+				conf.Schedules[0].Tasks[0].Repo = "https://github.com/jshiv/cronicle-sample.git"
+				repos := config.GetRepos(&conf)
+				expected := map[string]bool{
+					"https://github.com/jshiv/cronicle-sample.git": true,
+				}
+				Expect(repos).To(Equal(expected))
+			})
+		})
+	})
+
+	Describe("get different repos from Config, Schedules and Tasks", func() {
+		Context("config.GetRepos", func() {
+			It("should be https://github.com/jshiv/cronicle-sample.git", func() {
+				conf := config.Default()
+				conf.Repos = []string{"https://github.com/jshiv/cronicle-sample.git"}
+				conf.Schedules[0].Repo = "https://github.com/jshiv/cronicle-sample1.git"
+				conf.Schedules[0].Tasks[0].Repo = "https://github.com/jshiv/cronicle-sample2.git"
+				repos := config.GetRepos(&conf)
+				expected := map[string]bool{
+					"https://github.com/jshiv/cronicle-sample.git":  true,
+					"https://github.com/jshiv/cronicle-sample1.git": true,
+					"https://github.com/jshiv/cronicle-sample2.git": true,
+				}
+				Expect(repos).To(Equal(expected))
+			})
+		})
+	})
+
 })
